d9e5eca5e8a205361d4bc5a6bfcd76c34ab758ec
ajcodes020 <108165407+ajcodes020@users.noreply.github.com>
2024-02-19 18:26:46 +0800

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,11 +1,15 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ('Courier', 12, 'normal')
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highest_score = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -13,13 +17,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
+        self.clear()
+        self.write(f"Score: {self.score}\tHighest Score: {self.highest_score}", False, ALIGNMENT, FONT)
     
-    def game_over(self):
-        self.home()
-        self.write("GAME OVER.", False, ALIGNMENT, FONT)
+    def reset_score(self):
+        if self.highest_score < self.score:
+            self.highest_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(f"{self.score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+    
+    # def game_over(self):
+    #     self.home()
+    #     self.write("GAME OVER.", False, ALIGNMENT, FONT)
